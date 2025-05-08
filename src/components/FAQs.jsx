c6d8eccbae67b1ccc4087b8838673af3fdb5d00e
lucascr <lcrodrigues1995@gmail.com>
2025-05-08 11:27:36 -0300

    Update FAQs.jsx

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -18,10 +18,10 @@ export default function FAQs({ faqs, type }) {
     <div className="bg-cod-gray-100">
       <div className="mx-auto max-w-7xl px-6 py-8 lg:py-24 sm:py-12 lg:pt-16 lg:pb-12">
         <div className="mx-auto max-w-4xl">
-          <p className="mx-auto mt-12 uppercase text-lg lg:text-xl lg:max-w-3xl font-sans text-center font-bold tracking-tight text-sky-400">
+          <p className="mx-auto mt-12 uppercase text-lg lg:text-xl lg:max-w-3xl font-sans text-center font-bold tracking-tight text-primary-400">
             {type}
           </p>
-          <h2 className="mx-auto mb-3 text-4xl lg:text-6xl lg:max-w-3xl font-title text-center font-bold tracking-tight text-sky-950">
+          <h2 className="mx-auto mb-3 text-4xl lg:text-6xl lg:max-w-3xl font-title text-center font-bold tracking-tight text-primary-950">
             Frequently Asked Questions
           </h2>
           <dl className="flex flex-col gap-2 mt-6">
@@ -40,7 +40,7 @@ export default function FAQs({ faqs, type }) {
           If you have any additional questions please send us an email to{" "}
           <a
             href={`mailto:${EMAIL_ADDRESS}`}
-            className="text-sky-500 hover:underline"
+            className="text-primary-500 hover:underline"
           >
             {EMAIL_ADDRESS}
           </a>
