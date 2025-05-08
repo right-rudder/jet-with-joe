ae895f8b63fde2d3dba79830b297b07141cf8b52
lucascr <lcrodrigues1995@gmail.com>
2025-05-08 15:15:25 -0300

    Update accordion.jsx

--- src/components/accordion.jsx
+++ src/components/accordion.jsx
@@ -4,23 +4,23 @@ const Accordion = ({ faq, toggled, onShow, index }) => {
   return (
     <div className="relative">
       {/* Header */}
-      <div className="flex flex-col items-start p-7 bg-gradient-to-br from-sky-200 to-sky-300 rounded-t-lg">
+      <div className="flex flex-col items-start p-7 bg-gradient-to-br from-primary-700 to-primary-800 rounded-t-lg">
         <div
           id={faq.title}
           onClick={onShow}
           className="absolute z-10 top-0 left-0 w-full h-32 cursor-pointer"
         ></div>
         <div className="w-full h-full flex items-center justify-between text-black py-2">
-          <span className="font-mono lg:mr-4 text-lg lg:text-5xl font-semibold w-6 lg:w-24 text-sky-800/30 tracking-tight overflow-hidden text-left">
+          <span className="font-mono lg:mr-4 text-lg lg:text-5xl font-semibold w-6 lg:w-24 text-cranberry-500 tracking-tight overflow-hidden text-left">
             {index + 1}
           </span>
-          <p className="text-base lg:text-xl text-sky-950 font-semibold w-full text-center lg:text-left">
+          <p className="text-base lg:text-xl text-accent-400 font-semibold w-full text-center lg:text-left">
             {faq.title}
           </p>
           {toggled ? (
-            <FaMinus className="mr-3 pointer-events-none text-sky-700/50" />
+            <FaMinus className="mr-3 pointer-events-none text-cranberry-500 text-xl" />
           ) : (
-            <FaPlus className="mr-3 pointer-events-none text-sky-700/50" />
+            <FaPlus className="mr-3 pointer-events-none text-cranberry-500 text-xl" />
           )}
         </div>
       </div>
